83c7b245b065d07bf1b7abc239c5c678e329ce75
rod156 <rodrigo.villacorta1@gmail.com>
2014-06-25 19:40:53 -0400

    Updated messages.

--- gduran144.py
+++ gduran144.py
@@ -2,10 +2,10 @@ from util import hook
 import random
 
 @hook.command
-def gduran144(inp, conn=None ):
+def gduran144(inp, conn=None, action=None):
         value = random.randint(0, 6)
         if value == 2:
-                conn.cmd("KICK #eisenstein GDuran144 Someone got you kicked!")
-                return "*BANG*";
+                action("*BANG*")
+                conn.cmd("KICK #eisenstein GDuran144 Do the Truffle Shuffle!");
         else:
                 return "He got lucky this time...";
