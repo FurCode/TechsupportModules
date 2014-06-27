f0bc771d9975b9fcef7837f61d1ce71eda949cc8
rod156 <rodrigo.villacorta1@gmail.com>
2014-06-27 14:58:53 -0400

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,7 +1,22 @@
 from util import hook
+import random
 
-@hook.command('welcome')
-def echo(inp):
-        if inp == "debug":
-                return "Welcome back /User/!"
-#test
+@hook.command('debugwelcome')
+@hook.event("JOIN")
+def welcome(inp, chan=None, nick=None):
+        if chan == "#eisenstein":
+                value = random.randint(0, 8)
+                valuestring = str(value)
+                dict = {'0': 'I\'m not really alive, by the way.'};
+                dict['1'] = "How are you today?";
+                dict['2'] = "I hope you feel well.";
+                dict['3'] = "It is good to see you back!";
+                dict['4'] = "Making some coffee for you!";
+                dict['5'] = "Really, welcome back!";
+                dict['6'] = "Umm, Chocolate or Vanilla?";
+                dict['7'] = "We missed you!";
+                dict['8'] = "Here, have a sandwich!";
+                comment = dict.get(valuestring, "Something went wrong");
+                return "Welcome back " + nick + "!" + " " + comment
+        else:
+                response = "This is not the right channel"
