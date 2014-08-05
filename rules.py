92cbde9c4044f648efe71c12b5baa8621d9355d9
hameerabbasi <hameerabbasi@yahoo.com>
2014-08-05 20:26:09 +0500

    Highlight user to whom rules are addressed

--- rules.py
+++ rules.py
@@ -12,4 +12,4 @@ def rules(inp, conn=None, chan=None):
 		if inp == "debug":
 			conn.cmd("PRIVMSG " + chan + " This is a debug message. Thank you for listening.");
 		else:
-			return "Welcome to #techsupport! | Do not ask for help or specific skills, just state your issue. | For verbose descriptions, use a pastebin or a link to your /r/techsupport thread. | Generic nicks must be changed by typing /nick <nickname>. | Please be patient and don't spam.";
+			return inp + ": Welcome to #techsupport! | Do not ask for help or specific skills, just state your issue. | For verbose descriptions, use a pastebin or a link to your /r/techsupport thread. | Generic nicks must be changed by typing /nick <nickname>. | Please be patient and don't spam.";
